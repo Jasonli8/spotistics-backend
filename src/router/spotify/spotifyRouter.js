d1c4aa52c205cf8235740a4302d9bd3529070aeb
Jason Li <li.js001@gmail.com>
2024-03-04 15:51:51 -0500

    Implemented getters for user profile and recommendations for prototype

--- src/router/spotify/spotifyRouter.js
+++ src/router/spotify/spotifyRouter.js
@@ -4,5 +4,7 @@ const router = express.Router()
 const spotify = require("@utils/service/spotify")
 
 router.get("/history", spotify.getListeningHistory)
+router.get("/user", spotify.getUserProfile)
+router.get("/popular", spotify.getPopularTracks)
 
 module.exports = router
--- src/utils/service/spotify.js
+++ src/utils/service/spotify.js
@@ -48,11 +48,83 @@ const getListeningHistory = async (req, res, next) => {
 
             res.status(200).send({ history, items: history.length })
         } else {
-            res.status(500).send({message: "Couldn't get history", code: 201})
+            res.status(500).send({message: "Couldn't get history", code: 201, error: body})
         }
     })
 }
 
+const getUserProfile = async (req, res, next) => {
+    var options = {
+        url: 'https://api.spotify.com/v1/me',
+        headers: {
+            Authorization: 'Bearer ' + (req.cookies && req.cookies["access-token"] ? req.cookies["access-token"] : "")
+        },
+    };
+
+	request.get(options, function(error, response, body) => {
+		if (!error && response.status == 200) {
+			res.status.200.json(body)
+		} else {
+			res.status(500).send({message: "Couldn't get user profile", code: 202, error: body})
+		}
+	})
+}
+
+const getPopularTracks = async (req, res, next) => {
+	var options = {
+        url: 'https://api.spotify.com/v1/recommendations?seed_tracks=0c6xIDDpzE81m2q797ordA&limit=10',
+        headers: {
+            Authorization: 'Bearer ' + (req.cookies && req.cookies["access-token"] ? req.cookies["access-token"] : "")
+        },
+    };
+
+	request.get(options, function(error, response, body) => {
+		if (!error && response.status == 200) {
+			var recommendations = JSON.parse(body).tracks.map(track => {
+                return {
+                    album: {
+                        id: track.album.id,
+                        name: track.album.name,
+                        images: track.album.images,
+                        release_date: track.album.release_date_precision,
+                        urls: track.album.external_urls,
+                        artists: track.album.artists.map((artist) => {
+                            return {
+                                id: artist.id,
+                                name: artist.name,
+                                urls: artist.external_urls
+                            }
+                        })
+                    },
+                    track: {
+                        duration: track.duration_ms,
+                        explicit: track.explicit,
+                        urls: track.external_urls,
+                        id: track.id,
+                        name: track.name,
+                        spotify_popularity: track.popularity,
+                        is_local_file: track.is_local,
+                        artists: track.artists.map((artist) => {
+                            return {
+                                id: artist.id,
+                                name: artist.name,
+                                urls: artist.external_urls
+                            }
+                        })
+                    }
+                }
+            })
+
+            res.status(200).send({ recommendations, items: recommendations.length })
+
+		} else {
+			res.status(500).send({message: "Couldn't get recommendations", code: 203, error: body})
+		}
+	})
+}
+
 module.exports = {
     getListeningHistory
+	getUserProfile
+	getPopularTracks
 }
